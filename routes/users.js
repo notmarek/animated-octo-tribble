815cabb4d3702b9dd8623cf1455f85dd297b4384
Marek Veselý <veselym@spstrutnov.cz>
2023-02-14 14:50:47 +0100

    cringe ass login

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ let router = express.Router();
 router.post('/register', async (req, res, next) => {
   let client = await connPromise;
   let collection = client.db("app").collection("users");
-  console.log({username: req.body.username, password: await argon2.hash(req.body.password), email: req.body.email});
   if (await (await collection.find({username: req.body.username})).first())
     return res.json({ok: false, msg: "This username is already taken."});
   if (await (await collection.find({email: req.body.email})).first())
@@ -15,6 +14,17 @@ router.post('/register', async (req, res, next) => {
   
   await collection.insertOne({username: req.body.username, password: await argon2.hash(req.body.password), email: req.body.email});
   res.json({ok: true, msg: "Account created."});
+});
+
+router.post("/login", async (req, res, next) => {
+  let client = await connPromise;
+  let collection = client.db("app").collection("users");
+  let found_user = await (await collection.find({username: req.body.username})).first();
+  if (!found_user)
+    return res.json({ok: false, msg: "This username doesn't seem to exist."});
+  if (!(await argon2.verify(found_user.password, req.body.password)))
+    return res.json({ok: false, msg: "Wrong password."});
+  return res.json({ok: true, msg: "Logged in.", token: "djkljslkjds"});
 });
 
 export default router
