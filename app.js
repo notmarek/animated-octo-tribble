4a162b90afb306a789e073ea11f36aba9c8996ae
Marek Veselý <veselym@spstrutnov.cz>
2023-02-14 14:15:54 +0100

    jello

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js" 
+import http from "http"
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join('public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.set('port', 3000);
+let server = http.createServer(app);
+server.listen(app.get("port"));
+server.on('listening', () => console.log("running on http://127.0.0.1:3000"));
