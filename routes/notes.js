f0b271f2214a2b90bab9453d1118cc082c7ef4fe
Marek Veselý <veselym@spstrutnov.cz>
2023-02-21 11:13:58 +0100

    notes

--- routes/notes.js
+++ routes/notes.js
@@ -27,6 +27,19 @@ router.get("/all", async (req, res, next) => {
     return res.json({ok: true, msg: "success", data: notes });
 })
 
+router.post("/star", async (req, res, next) => {
+    let jwt = req.headers.authorization;
+    const secret = req.app.get("secret");
+    let jwt_res = verify_jwt(secret, jwt);
+    if (!jwt_res)
+        return res.json({ok: false, msg: "Invalid token"});
+    let client = await connPromise;
+    let collection = client.db("app").collection("notes");
+    let is_starred = (await (await collection.find({ user_id: jwt_res.payload.user_id, _id: req.body.note_id })).first()).important;
+    await collection.updateOne({ user_id: jwt_res.payload.user_id, _id: req.body.note_id }, {important: !is_starred});
+    return res.json({ok: true, msg: "success" }); 
+})
+
 
 
 export default router;
